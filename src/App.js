2e498c724813c7340d18b25bf33fc07dc36c9fd9
Ruddy ROZAS <ruddyrozas@Mac-mini-de-Ruddy.local>
2023-01-12 17:16:31 -0400

    feat: add basic style

--- src/App.js
+++ src/App.js
@@ -4,20 +4,40 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Page-wrapper">
+        <button style={{ position: 'sticky', top: '.5em', right: '.5em', marginLeft: 'auto' }} className="Button">
+          Open modal
+        </button>
+        <h2>Articles en vente ici.</h2>
+        <div className="Wrapper">
+          <div className="Products">
+            <div className="Product Row-wrapper">
+              <img className="Picture" src={logo} alt="test"/>
+              <div className="Product-content Wrapper">
+                <h3>Nom de l'article</h3>
+                <span style={{ display: 'block', fontSize: '1.8em' }} >Prix €</span>
+                <button className="Button">Action</button>
+              </div>
+            </div>
+          </div>
+          <div className="Modal-wrapper disabled">
+            <span style={{ position: 'static', top: '.5em', right: '.5em', marginLeft: 'auto' }} className='close'>
+              Ⓧ
+            </span>
+            <h2>Mon panier</h2>
+            <div className="Modal-content">
+              <div className="Product Row-wrapper">
+                <img className="Picture" src={logo} alt="test"/>
+                <div className="Product-content Wrapper">
+                  <h3>Nom de l'article</h3>
+                  <span style={{ display: 'block', fontSize: '1.8em' }} >Prix €</span>
+                  <button className="Button">Action</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
