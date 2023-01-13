fce87a2ff0416bb1d9665f70f1fc4172f6241e23
Ruddy ROZAS <ruddyrozas@Mac-mini-de-Ruddy.local>
2023-01-13 09:23:07 -0400

    feat: add media on produc view

--- src/Components/ProductDetail.jsx
+++ src/Components/ProductDetail.jsx
@@ -6,13 +6,9 @@ import React from 'react'
  * @returns 
  */
 const ProductDetail = ({ name, picture, price, disabled, action, callback }) => {
-  const  productPicture = picture ?
-    <img className="Picture" src={picture} alt={name} /> :
-    null
-
   return (
     <div className={['Product', 'Row-wrapper', disabled ? 'disabled' : null].join(' ')}>
-      {productPicture}
+      <img className="Picture" src={picture} alt={name} />
       <div className={['Product-content', 'Wrapper'].join(' ')}>
         <h3>{name}</h3>
         <span style={{ display: 'block', fontSize: '1.8em' }} >{price} €</span>
--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -9,9 +9,9 @@ const ProductList = ({ addOnCart }) => {
   // Returns a random integer from 100 to 10000:
   const getRandomInt = () => Math.floor(Math.random() * (10000 - 100 + 1) ) + 100
   const [products] = useState([
-    { id: Math.random().toString(), product: undefined, name: 'Une paire de chausure', price: getRandomInt() },
-    { id: Math.random().toString(), product: undefined, name: 'Appareil photo', price: getRandomInt() },
-    { id: Math.random().toString(), product: undefined, name: 'Une monte connecter', price: getRandomInt() },
+    { id: Math.random().toString(), product: require('../media/shoes.jpg'), name: 'Une paire de chausure', price: getRandomInt() },
+    { id: Math.random().toString(), product: require('../media/camera.jpg'), name: 'Appareil photo', price: getRandomInt() },
+    { id: Math.random().toString(), product: require('../media/watch.jpg'), name: 'Une monte connecter', price: getRandomInt() },
   ])
 
   return (
