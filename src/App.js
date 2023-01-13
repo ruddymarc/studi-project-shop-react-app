d619bca22414e5132b486aff3f50b3d5f9d3ff7f
Ruddy ROZAS <ruddyrozas@Mac-mini-de-Ruddy.local>
2023-01-12 20:16:07 -0400

    feat: add modal wrapper component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Page from './Components/Page'
+import Modal from './Components/Modal'
 import ProductList from './Components/ProductList';
 import logo from './logo.svg';
 import './App.css';
@@ -8,22 +9,16 @@ function App() {
     <div className="App">
       <Page title='Articles en vente ici.' action='Open modal'>
         <ProductList />
-        <div className="Modal-wrapper disabled">
-          <span style={{ position: 'static', top: '.5em', right: '.5em', marginLeft: 'auto' }} className='close'>
-            Ⓧ
-          </span>
-          <h2>Mon panier</h2>
-          <div className="Modal-content">
-            <div className="Product Row-wrapper">
-              <img className="Picture" src={logo} alt="test"/>
-              <div className="Product-content Wrapper">
-                <h3>Nom de l'article</h3>
-                <span style={{ display: 'block', fontSize: '1.8em' }} >Prix €</span>
-                <button className="Button">Action</button>
-              </div>
+        <Modal title='Mon panier'>
+          <div className="Product Row-wrapper">
+            <img className="Picture" src={logo} alt="test"/>
+            <div className="Product-content Wrapper">
+              <h3>Nom de l'article</h3>
+              <span style={{ display: 'block', fontSize: '1.8em' }} >Prix €</span>
+              <button className="Button">Action</button>
             </div>
           </div>
-        </div>
+        </Modal>
       </Page>
     </div>
   );
---
+++ src/Components/Modal.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Modal = ({ title, children, displayed, closeAction }) => {
+  return (
+    <div className={['Modal-wrapper', displayed ? 'disabled' : null].join(' ')}>
+      <span 
+        className='close'
+        style={{ position: 'static', top: '.5em', right: '.5em', marginLeft: 'auto' }} 
+        onClick={closeAction}
+      >
+        Ⓧ
+      </span>
+      <h2>{title}</h2>
+      <div className="Modal-content">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Modal
